f042e999b3089c59bf9f3279ae01f346bb6caa0c
Adam <noneya@busi.ness>
2019-06-03 20:51:58 -0400

    change namespace for local storage

--- src/bottle.js
+++ src/bottle.js
@@ -1,3 +1,5 @@
+const NAMESPACE = "blockchainLedgerState";
+
 function bottle (state) {
     let data = JSON.stringify(state);
     return data;
@@ -15,7 +17,7 @@ function unbottle (stateString) {
 
 export function getStateFromStorage () {
     if (localStorage) {
-        let stateFromStorage = unbottle(localStorage.getItem("state"))
+        let stateFromStorage = unbottle(localStorage.getItem(NAMESPACE))
         if (stateFromStorage !== null) {
             return stateFromStorage
         }
@@ -24,5 +26,5 @@ export function getStateFromStorage () {
 }
 
 export function persistStateToStorage (state) {
-    localStorage.setItem("state", bottle(state));
+    localStorage.setItem(NAMESPACE, bottle(state));
 }
