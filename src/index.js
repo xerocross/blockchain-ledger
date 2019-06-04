0d3ef5227324ce40bcd9abd7b1b2357e6b620de5
Adam <noneya@busi.ness>
2019-06-03 20:51:07 -0400

    rename root element
    rename root element

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('blockchain-demo'));
+ReactDOM.render(<App />, document.getElementById('blockchain-ledger'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
